47afe2b26eaf53ec5d7fd5be06b25337794311e6
Pooja Patel <poojapatel803@gmail.com>
2024-12-19 19:48:35 -0600

    adding jwt token feature

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask, request, jsonify
 from sqlalchemy.exc import SQLAlchemyError
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from models import db, Student  
-
 
 ## add oAuth token feature
 
@@ -10,9 +10,13 @@ app = Flask(__name__)
 
 # Configure the database URI (use SQLite for this example)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///invalid_path/students.db'
+app.config['SECRET_KEY'] = 'secret_key1'
 
 # Initialize the database
 db.init_app(app)
+jwt = JWTManager(app)
+
 
 # Create tables before first request (if not already created)
 @app.before_request
@@ -29,15 +33,27 @@ def create_tables():
         print(f"Unexpected error: {str(e)}")
         return jsonify({"error": "Unexpected error occurred during database initialization", "details": str(e)}), 500
 
+# Endpoint to general JWT Token
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    if data.get('username') == 'admin' and data.get('password') == 'password':
+        access_token = create_access_token(identity=data['username'])
+        return jsonify(access_token=access_token), 200
+
+    return jsonify({"error": "invalid credentials"}), 401
 
 # 1. Get all students
 @app.route('/students', methods=['GET'])
+@jwt_required()
 def get_students():
     students = Student.query.all()  # Get all students from the database
     return jsonify([student.to_dict() for student in students])  # Convert to dict and return as JSON
 
 # 2. Insert a new student
 @app.route('/students', methods=['POST'])
+@jwt_required()
 def add_student():
     data = request.get_json()
     first_name = data.get('first_name')
@@ -57,6 +73,7 @@ def add_student():
 
 # 3. Update a student's information
 @app.route('/students/<int:student_id>', methods=['PUT'])
+@jwt_required()
 def update_student(student_id):
     data = request.get_json()
     student = Student.query.get(student_id)  # Get student by ID
